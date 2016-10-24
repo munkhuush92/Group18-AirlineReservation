68651658aab02e5300b855d7100181747d5c6f0e
Austin Ingraham <adi1996@uw.edu>
2016-10-24 15:41:21 -0700

    Initial commit.

---
+++ app/src/main/java/uw/group18_airlinereservation/MainActivity.java
@@ -0,0 +1,13 @@
+package uw.group18_airlinereservation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
