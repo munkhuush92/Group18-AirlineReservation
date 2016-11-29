bf5631a137467584d0f653b7b6244e3419914096
Munkhbayar Ganbold <king23_gmb@yahoo.com>
2016-11-29 10:24:26 -0800

    Added two radio buttons

--- app/src/main/java/uw/group18_airlinereservation/MainActivity.java
+++ app/src/main/java/uw/group18_airlinereservation/MainActivity.java
@@ -1,13 +1,31 @@
 package uw.group18_airlinereservation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    /** A button for directing to Booking layout*/
+     Button bookButton;
+
+    public void init(){
+        bookButton = (Button) findViewById(R.id.bookButton);
+        bookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent initialize = new Intent(MainActivity.this, booking.class);
+                startActivity(initialize);
+
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
 }
