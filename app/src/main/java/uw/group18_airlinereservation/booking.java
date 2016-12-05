89968402ed6e21420c421d0709589334dbb801a2
Munkhbayar Ganbold <king23_gmb@yahoo.com>
2016-12-05 11:20:33 -0800

    added search button and result
    need to add result using insert into mysql

--- app/src/main/java/uw/group18_airlinereservation/booking.java
+++ app/src/main/java/uw/group18_airlinereservation/booking.java
@@ -1,15 +1,39 @@
 package uw.group18_airlinereservation;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
+
+import static android.R.attr.button;
+import static android.R.attr.data;
 
 /**
  * Created by admin on 11/28/2016.
@@ -27,16 +51,44 @@ public class booking extends AppCompatActivity {
     Spinner retSpinnerMonth;
     Spinner retSpinnerYear;
     ArrayList<CheckBox> myCheckedBoxList;
-    RadioGroup radioGroupFlights;
+    ListView myBookFlights;
+    ArrayAdapter<String> myAdapter;
+    String[] myJsonDatas;
+
+    /** Autocomplete textviews for FROM AND TO*/
+    AutoCompleteTextView fromTextView;
+    AutoCompleteTextView toTextView;
+    Button searchButton;
+
+    /** textview strings that are used for search results   */
+    String depString;
+    String arrString;
+    /** DO-TO   */
+    String depDateString;
+    String arrDateString;
 
 
 
+    /** Address of the server  */
+    String myurl = "http://10.0.2.2/data/test/fetch.php";
 
+    /** input stream for json data */
+    InputStream myInputStream = null;
+
+    /** Each line that is being read from database */
+    String myLine = null;
+
+    /** the result of converted string from the database */
+    String result;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.booking);
         //Initiliaze Dep and Return spinners
+        depSpinnerDay = (Spinner) findViewById(R.id.daysSpinner);
+        depSpinnerMonth = (Spinner)findViewById(R.id.monthsSpinner);
+        depSpinnerYear = (Spinner)findViewById(R.id.yearsSpinner);
+
         retSpinnerDay = (Spinner)findViewById(R.id.daysSpinner2);
         retSpinnerMonth = (Spinner)findViewById(R.id.monthsSpinner2);
         retSpinnerYear = (Spinner)findViewById(R.id.yearsSpinner2);
@@ -45,11 +97,84 @@ public class booking extends AppCompatActivity {
         retSpinnerMonth.setEnabled(false);
         retSpinnerYear.setEnabled(false);
 
+
         addListenerOnButton();
-        /** initiliaze checkboxes given number of items */
-        // change counter with size of given list of flights
+        /** initiliaze list views with full of flight options
+         *
+         */
+        myBookFlights = (ListView)findViewById(R.id.listViewflights);
+        StrictMode.setThreadPolicy((new StrictMode.ThreadPolicy.Builder().permitNetwork().build()));
+
+        //initiaze auto text views
+        toTextView = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextViewTO);
+        fromTextView = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextViewFROM);
+        updateJson();
 
 
+
+
+    }
+
+
+
+    private void updateJson(){
+        //Retrieve our data
+        getData();
+        //Adapter
+        myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myJsonDatas);
+    }
+
+    private void getData(){
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            myInputStream = new BufferedInputStream(con.getInputStream());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // CONVERT INTO STRING
+        try{
+            BufferedReader buffReader = new BufferedReader(new InputStreamReader(myInputStream));
+            StringBuilder sb  = new StringBuilder();
+            while((myLine=buffReader.readLine())!=null){
+
+                sb.append(myLine+"\n");
+
+            }
+            buffReader.close();
+            result = sb.toString();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        // parse json data
+        try{
+            JSONArray myJsonArr = new JSONArray(result);
+            JSONObject jsonObject = null;
+            myJsonDatas = new String[myJsonArr.length()];
+
+            for(int i =0 ; i < myJsonArr.length(); i++){
+                jsonObject = myJsonArr.getJSONObject(i);
+
+                if(jsonObject.getString("dep").equals(depString) && jsonObject.getString("arr").equals(arrString)) {
+                    String fullText = "" + jsonObject.getString("airplanename") +"/Departs From: "+ depString + "/Dep Date: " +
+                            jsonObject.getString("deptime").substring(5,16)+"/Arrive To: "+arrString+"/Arrival Date: "
+                            +jsonObject.getString("arrtime").substring(5,16)+"/ Price: "+jsonObject.getString("price").substring(0,5)+"$" ;
+
+                    myJsonDatas[i] = fullText;
+               }else {
+                    myJsonDatas[i] = "";
+                }
+
+            }
+        }catch(Exception e){
+                e.printStackTrace();
+
+        }
     }
     public void addListenerOnButton() {
 
@@ -72,14 +197,28 @@ public class booking extends AppCompatActivity {
                 retSpinnerYear.setEnabled(true);
             }
         });
-        radioGroupFlights =(RadioGroup) findViewById(R.id.radioGroupFlights);
+        searchButton = (Button) findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               depString = fromTextView.getText().toString();
 
+                arrString= toTextView.getText().toString();
+                depDateString = depSpinnerDay.getSelectedItem().toString() + " / "+ depSpinnerMonth.getSelectedItem().toString()
+                                        +" "+ depSpinnerYear.getSelectedItem().toString();
 
+                 updateJson();
+                myBookFlights.setAdapter(myAdapter);
+
+            }
+        });
 
 
 
 
     }
+
+
 
 
 
